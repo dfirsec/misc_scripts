554ffe3de9b48bf0e1f5294782374f9466b16359
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-05-23 12:49:55 -0400

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3,41 +3,41 @@
 # Credit: JohnHammond
 # https://github.com/JohnHammond/ignition_key/blob/master/ignition_key.sh
 
-# Define colors
-ERROR=$(tput bold && tput setaf 1)
-SUCCESS=$(tput bold && tput setaf 2)
-WARNING=$(tput bold && tput setaf 3)
-PROCESSING=$(tput bold && tput setaf 6)
-RESET=$(tput sgr0)
+# Define colors ]
+error=$(tput bold && tput setaf 1)
+success=$(tput bold && tput setaf 2)
+info=$(tput bold && tput setaf 3)
+processing=$(tput bold && tput setaf 6)
+reset=$(tput sgr0)
 
-ERROR() {
-	echo -e "\n${ERROR}[ERROR] ${1}${RESET}"
+error() {
+	echo -e "\n${error}[ERROR] ${1}${reset}"
 }
-SUCCESS() {
-	echo -e "\n${SUCCESS}[SUCCESS] ${1}${RESET}"
+success() {
+	echo -e "\n${success}[SUCCESS] ${1}${reset}"
 }
-WARNING() {
-	echo -e "\n${WARNING}[WARNING] ${1}${RESET}"
+info() {
+	echo -e "\n${info}[INFO] ${1}${reset}"
 }
-PROCESSING() {
-	echo -e "\n${PROCESSING}${1}${RESET}"
+processing() {
+	echo -e "\n${processing}${1}${reset}"
 }
 
 # check if ran as sudo
 if [ "$EUID" -eq 0 ]; then
-	ERROR "Please do not run as root" && echo
+	error "Please do not run as root" && echo
 	exit
 fi
 
-UPDATE() {
+update_sys() {
 	sudo apt update
 	sudo apt upgrade -y
 }
 
-PROCESSING "[ Forcing color prompt in ~/.bashrc ]"
+processing "[ Forcing color prompt in ~/.bashrc ]"
 echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[38;5;11m\]\u\[$(tput sgr0)\]@\h:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]\[$(tput sgr0)\]: \[$(tput sgr0)\]'" >>~/.bashrc
 
-INSTALL_CHECK() {
+install_check() {
 	progs=(snapd software-properties-common apt-transport-https code git terminator taskwarrior python3-pip build-essential libssl-dev libffi-dev python3-dev guake openvpn nmap docker.io curl pinta libimage-exiftool-perl python-pil sqlitebrowser wireshark binwalk tesseract-ocr foremost idle xclip bsdgames hexedit golang-go gccgo-go sqlite nikto sqlite nikto zbar-tools qrencode pdfcrack virtualbox-qt vagrant ffmpeg fcrackzip unrar p7zip steghide gimp cmake mplayer sshpass tcpflow libcompress-raw-lzma-perl sublime-text simplescreenrecorder stegsolve hashcat vnc gobuster font-manager openjdk-11-jre-headless ghidra volatility3)
 	for name in "${progs[@]}"; do
 		dpkg -s "$name" &>/dev/null
@@ -48,9 +48,9 @@ INSTALL_CHECK() {
 			#   vscode
 			############################
 			if [[ $name == "code" ]]; then
-				PROCESSING "[ Importing the Microsoft GPG key ]"
+				processing "[ Importing the Microsoft GPG key ]"
 				wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-				PROCESSING "[ Enabling the Visual Studio Code repository and install ]"
+				processing "[ Enabling the Visual Studio Code repository and install ]"
 				sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 				sudo apt update
 				sudo apt install code -y
@@ -59,7 +59,7 @@ INSTALL_CHECK() {
 			#   docker
 			############################
 			elif [[ $name == "docker.io" ]]; then
-				PROCESSING "[ Installing Docker ]"
+				processing "[ Installing Docker ]"
 				sudo apt install docker.io -y
 				sudo groupadd docker
 				sudo usermod -aG docker "$(logname)"
@@ -68,7 +68,7 @@ INSTALL_CHECK() {
 			#   atom
 			############################
 			elif [[ $name == "atom" ]]; then
-				PROCESSING "[ Installing Atom ]"
+				processing "[ Installing Atom ]"
 				wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 				sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 				sudo apt update
@@ -78,7 +78,7 @@ INSTALL_CHECK() {
 			#   xclip
 			############################
 			elif [[ $name == "xclip" ]]; then
-				PROCESSING "[ Installing xclip ]"
+				processing "[ Installing xclip ]"
 				sudo apt install -y xclip
 				grep "alias xclip" ~/.bashrc
 				if [ $? -eq 1 ]; then
@@ -89,7 +89,7 @@ INSTALL_CHECK() {
 			#   sublime
 			############################
 			elif [[ $name == "sublime-text" ]]; then
-				PROCESSING "[ Installing Sublime Text ]" # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
+				processing "[ Installing Sublime Text ]" # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
 				wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 				echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 				sudo apt update
@@ -99,7 +99,7 @@ INSTALL_CHECK() {
 			#  simplescreenrecorder
 			############################
 			elif [[ $name == "simplescreenrecorder" ]]; then
-				PROCESSING "[ Installing SimpleScreenRecorder ]"
+				processing "[ Installing SimpleScreenRecorder ]"
 				echo "" | sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
 				sudo apt update
 				sudo apt install simplescreenrecorder -y
@@ -111,7 +111,7 @@ INSTALL_CHECK() {
 				if [ -f "stegsolve.jar" ]; then
 					echo 'skipping' &>/dev/null
 				else
-					PROCESSING "[ Downloading stegsolve.jar ]"
+					processing "[ Downloading stegsolve.jar ]"
 					wget "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
 					chmod +x "stegsolve.jar"
 				fi
@@ -123,7 +123,7 @@ INSTALL_CHECK() {
 				if [[ -x $(command -v hashcat) ]]; then
 					echo 'skipping' &>/dev/null
 				else
-					PROCESSING "[ Installing hashcat ]"
+					processing "[ Installing hashcat ]"
 					wget https://hashcat.net/files/hashcat-5.1.0.7z
 					p7zip -d hashcat-5.1.0.7z
 					cd hashcat-5.1.0 || exit
@@ -141,17 +141,17 @@ INSTALL_CHECK() {
 				if [ $? -eq 0 ]; then
 					echo "$name is installed" &>/dev/null
 				else
-					PROCESSING "[ Install Real VNC Viewer ]"
+					processing "[ Install Real VNC Viewer ]"
 					wget "https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.20.113-Linux-x64.deb" -O vnc_viewer.deb
 					dpkg -i vnc_viewer.deb
 					rm vnc_viewer.deb
 
-					PROCESSING "[ Install Real VNC Connect (Server) ]"
+					processing "[ Install Real VNC Connect (Server) ]"
 					wget 'https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.7.1-Linux-x64.deb' -O vnc_server.deb
 					dpkg -i vnc_server.deb
 					rm vnc_server.deb
 
-					PROCESSING "[ Adding VNC Connect (Server) service to the default startup /etc/rc.local ]"
+					processing "[ Adding VNC Connect (Server) service to the default startup /etc/rc.local ]"
 					grep "vncserver-x11-serviced.service" /etc/rc.local
 					if [ $? -eq 1 ]; then
 						echo "systemctl start vncserver-x11-serviced.service" >>~/etc/rc.local
@@ -162,7 +162,7 @@ INSTALL_CHECK() {
 			#   snapd
 			############################
 			elif [[ $name == "snapd" ]]; then
-				PROCESSING "[ Installing Snap ]"
+				processing "[ Installing Snap ]"
 				sudo apt install snapd -y
 
 				snap_progs=(spotify volatility-phocean)
@@ -171,10 +171,10 @@ INSTALL_CHECK() {
 					if [ $? -eq 0 ]; then
 						echo 'skipping' &>/dev/null
 					else
-						PROCESSING "[ Installing Spotify ]"
+						processing "[ Installing Spotify ]"
 						sudo snap install spotify
 
-						PROCESSING "[ Installing volatility ]"
+						processing "[ Installing volatility ]"
 						sudo snap install volatility-phocean
 					fi
 				done
@@ -186,13 +186,13 @@ INSTALL_CHECK() {
 				ghidra_dir="/opt/ghidra"
 				jdk_dir="/opt/jdk-11"
 				if [[ -d $ghidra_dir ]]; then
-					WARNING "ghidra already installed here: $ghidra_dir"
+					info "ghidra already installed here: $ghidra_dir"
 				else
 					wget 'https://ghidra-sre.org/ghidra_9.1.2_PUBLIC_20200212.zip' --no-hsts
 					sudo unzip -q ghidra_9.1.2_PUBLIC_20200212.zip -d /opt && sudo mv /opt/ghidra_* /opt/ghidra
 				fi
 				if [[ -d $jdk_dir ]]; then
-					WARNING "jdk-11 already installed here: $jdk_dir"
+					info "jdk-11 already installed here: $jdk_dir"
 				else
 					wget 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.7%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.7_10.tar.gz' --no-hsts
 					sudo mkdir -p /opt/jdk-11/ && sudo tar -xzf OpenJDK11U-jdk_x64_linux_hotspot_11.0.7_10.tar.gz -C /opt/jdk-11/ --strip-components 1
@@ -206,7 +206,7 @@ INSTALL_CHECK() {
 				if [[ -d $vol_dir ]]; then
 					echo 'skipping' &>/dev/null
 				else
-					PROCESSING "[ Downloading volatility3 ]"
+					processing "[ Downloading volatility3 ]"
 					git clone https://github.com/volatilityfoundation/volatility3.git
 				fi
 
@@ -214,58 +214,67 @@ INSTALL_CHECK() {
 			#   process all others
 			############################
 			else
-				PROCESSING "[ Installing $name ]"
+				processing "[ Installing $name ]"
 				sudo apt install "$name" -y
 			fi
 		fi
 	done
 }
 
-PROCESSING "[ Updating repositories ]"
-UPDATE
+#  pip installations
+pip_installs() {
+	pip_progs=(requests flask flask-login colorama passlib pwntools netifaces iptools pyopenssl pydispatch scapy pefile)
+	for name in "${pip_progs[@]}"; do
+		python3 -c "import $name" &>/dev/null
+		if [ $? -eq 0 ]; then
+			echo 'skipping' &>/dev/null
+		else
+			processing "[ Installing $name ]"
+			pip3 install "$name"
+		fi
+	done
+}
 
-PROCESSING "[ Checking Installed Software ]"
-INSTALL_CHECK
+# setup paths
+setup_paths() {
+	processing "[ Adding GOPATH and GOBIN to .bashrc ]"
+	grep "export GOPATH" ~/.bashrc
+	if [ $? -eq 1 ]; then
+		echo "export GOPATH=\$HOME/.go/" >>~/.bashrc
+	fi
+	grep "export GOBIN" ~/.bashrc
+	if [ $? -eq 1 ]; then
+		echo "export GOBIN=\$HOME/.go/bin" >>~/.bashrc
+		echo "export PATH=\$PATH:\$GOBIN" >>~/.bashrc
+	fi
+}
 
-PROCESSING "[ Setting terminator as the default terminal emulator ]"
+# remove boilerplate directories
+remove_dirs() {
+	bp_dirs=("$HOME/Desktop $HOME/Documents $HOME/Downloads $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos")
+	for name in "${bp_dirs[@]}"; do
+		if [ -d "$name" ]; then
+			processing "[ Removing boilerplate home directories ]"
+			rmdir "$name"
+		fi
+	done
+}
+
+processing "[ Updating repositories ]"
+update_sys
+
+processing "[ Checking Installed Software ]"
+install_check
+
+processing "[ Installing Python Modules ]"
+pip_installs
+
+processing "[ Setting terminator as the default terminal emulator ]"
 sed -i s/Exec=gnome-terminal/Exec=terminator/g /usr/share/applications/gnome-terminal.desktop
 
-bp_dirs=("$HOME/Desktop $HOME/Documents $HOME/Downloads $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos")
-for name in "${bp_dirs[@]}"; do
-	if [ -d "$name" ]; then
-		PROCESSING "[ Removing boilerplate home directories ]"
-		rmdir "$name"
-	fi
-done
+processing "[ Setting up Paths ]"
+setup_paths
 
-############################
-#   pip installations
-############################
-pip_progs=(requests flask flask-login colorama passlib pwntools netifaces iptools pyopenssl pydispatch scapy pefile)
-for name in "${pip_progs[@]}"; do
-	python3 -c "import $name" &>/dev/null
-	if [ $? -eq 0 ]; then
-		echo 'skipping' &>/dev/null
-	else
-		PROCESSING "[ Installing $name ]"
-		pip3 install "$name"
-	fi
-done
-
-############################
-#   setup paths
-############################
-PROCESSING "[ Adding GOPATH and GOBIN to .bashrc ]"
-grep "export GOPATH" ~/.bashrc
-if [ $? -eq 1 ]; then
-	echo "export GOPATH=\$HOME/.go/" >>~/.bashrc
-fi
-grep "export GOBIN" ~/.bashrc
-if [ $? -eq 1 ]; then
-	echo "export GOBIN=\$HOME/.go/bin" >>~/.bashrc
-	echo "export PATH=\$PATH:\$GOBIN" >>~/.bashrc
-fi
-
-PROCESSING "[ Updating Prompt ]"
+processing "[ Updating Prompt ]"
 echo "Done!"
 exec bash
