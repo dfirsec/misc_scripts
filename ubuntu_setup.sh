a4894973e447b19be74fefb3f77a7b90a9bbab58
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-05-23 08:21:59 -0400

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# CERRORit: JohnHammond
+# Credit: JohnHammond
 # https://github.com/JohnHammond/ignition_key/blob/master/ignition_key.sh
 
 # Define colors ]
@@ -10,9 +10,9 @@ WARNING=$(tput bold && tput setaf 3)
 PROCESSING=$(tput bold && tput setaf 6)
 RESET=$(tput sgr0)
 
-UPDATE(){
-    sudo apt update
-    sudo apt upgrade -y
+UPDATE() {
+	sudo apt update
+	sudo apt upgrade -y
 }
 
 ERROR() {
@@ -28,42 +28,108 @@ PROCESSING() {
 	echo -e "\n${PROCESSING}${1}${RESET}"
 }
 
+INSTALL_CHECK() {
+	progs=("snapd software-properties-common apt-transport-https code git terminator taskwarrior python-pip3 guake openvpn nmap docker.io curl pinta exiftool python-pil sqlitebrowser wireshark binwalk tesseract-ocr foremost idle pefile xclip bsdgames hexedit golang-go gccgo-go sqlite nikto sqlite nikto zbar-tools qrencode pdfcrack virtualbox-qt vagrant oracle-java8-installer ffmpeg fcrackzip unrar p7zip steghide gimp cmake mplayer sshpass tcpflow libcompress-raw-lzma-perl sublime-text simplescreenrecorder stegsolve.jar hashcat vnc_viewer.deb")
+	for name in "${progs[@]}"; do
+		if ! [ -x "$(command -v "$name")" ]; then
+			PROCESSING "[ Installing $name ]"
+			if [[ $name == "code" ]]; then
+				PROCESSING "[ Importing the Microsoft GPG key ]"
+				wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+				PROCESSING "[ Enabling the Visual Studio Code repository and install ]"
+				sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+				sudo apt update
+				sudo apt install code -y
+			elif [[ $name == "docker.io" ]]; then
+				PROCESSING "[ Installing Docker ]"
+				sudo apt install docker.io -y
+				sudo groupadd docker
+				sudo usermod -aG docker "$(logname)"
+			elif [[ $name == "atom" ]]; then
+				PROCESSING "[ Installing Atom ]"
+				wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+				sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+				sudo apt update
+				sudo apt install atom -y
+			elif [[ $name == "xclip" ]]; then
+				PROCESSING "[ Installing xclip ]"
+				sudo apt install -y xclip
+				grep "alias xclip" ~/.bashrc
+				if [ $? -eq 1 ]; then
+					echo "alias xclip='xclip -selection clipboard'" >>~/.bashrc
+				fi
+			elif [[ $name == "oracle-java8-installer" ]]; then
+				PROCESSING "[ Installing Oracle Java 8 ]"
+				echo "" | sudo add-apt-repository ppa:webupd8team/java
+				sudo apt update
+				sudo apt install oracle-java8-installer -y
+			elif [[ $name == "sublime-text" ]]; then
+				PROCESSING "[ Installing Sublime Text ]" # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
+				wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+				echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+				sudo apt update
+				sudo apt install sublime-text -y
+			elif [[ $name == "simplescreenrecorder" ]]; then
+				PROCESSING "[ Installing SimpleScreenRecorder ]"
+				echo "" | sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
+				sudo apt update
+				sudo apt install simplescreenrecorder -y
+			elif [[ $name == "stegsolve.jar" ]]; then
+				PROCESSING "[ Downloading stegsolve.jar ]"
+				wget "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
+				chmod +x "stegsolve.jar"
+			elif [[ $name == "hashcat" ]]; then
+				PROCESSING "[ Installing hashcat ]"
+				wget https://hashcat.net/files/hashcat-5.1.0.7z
+				p7zip -d hashcat-5.1.0.7z
+				cd hashcat-5.1.0 || exit
+				cp hashcat64.bin /usr/bin/
+				ln -s /usr/bin/hashcat64.bin /usr/bin/hashcat
+			elif [[ $name == "vnc_viewer.deb" ]]; then
+				PROCESSING "[ Install Real VNC Viewer ]"
+				wget "https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.17.1113-Linux-x64.deb" -O vnc_viewer.deb
+				dpkg -i vnc_viewer.deb
+				rm vnc_viewer.deb
+
+				PROCESSING "[ Install Real VNC Connect (Server) ]"
+				wget 'https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.2.1-Linux-x64.deb' -O vnc_server.deb
+				dpkg -i vnc_server.deb
+				rm vnc_server.deb
+
+				PROCESSING "[ Adding VNC Connect (Server) service to the default startup /etc/rc.local ]"
+				grep "vncserver-x11-serviced.service" /etc/rc.local
+				if [ $? -eq 1 ]; then
+					echo "systemctl start vncserver-x11-serviced.service" >>~/etc/rc.local
+				fi
+			elif [[ $name == "snapd" ]]; then
+				PROCESSING "[ Installing Snap ]"
+				sudo apt install snapd -y
+
+				PROCESSING "[ Installing Spotify ]"
+				sudo snap install spotify
+
+				PROCESSING "[ Installing volatility ]"
+				sudo snap install volatility-phocean
+			else
+				sudo apt install "$name" -y
+			fi
+		else
+			echo "$name already installed, skipping..."
+		fi
+	done
+}
+
 # Testing if root ]
 if [ $UID -ne 0 ]; then
 	ERROR "You must run this script as root!" && echo
 	exit
 fi
 
-
 PROCESSING "[ Updating repositories ]"
 UPDATE
 
-PROCESSING "[ Installing Snap ]"
-sudo apt install snapd
-
-PROCESSING "[ Installing Visual Studio Code ]"
-sudo apt install software-properties-common apt-transport-https -y
-
-PROCESSING "[ Importing the Microsoft GPG key ]"
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-
-PROCESSING "[ Enabling the Visual Studio Code repository and install ]"
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code -y
-
-PROCESSING "[ Installing git ]"
-sudo apt install git -y
-
-PROCESSING "[ Installing Sublime Text ]" # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install -y apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install sublime-text -y
-
-PROCESSING "[ Installing terminator ]"
-sudo apt install terminator -y
+PROCESSING "[ Checking Installed Software ]"
+INSTALL_CHECK
 
 PROCESSING "[ Setting terminator as the default terminal emulator ]"
 sed -i s/Exec=gnome-terminal/Exec=terminator/g /usr/share/applications/gnome-terminal.desktop
@@ -71,133 +137,33 @@ sed -i s/Exec=gnome-terminal/Exec=terminator/g /usr/share/applications/gnome-ter
 PROCESSING "[ Forcing color prompt in ~/.bashrc ]"
 echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[38;5;11m\]\u\[$(tput sgr0)\]@\h:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]\[$(tput sgr0)\]: \[$(tput sgr0)\]'" >>~/.bashrc
 
-PROCESSING "[ Installing SimpleScreenRecorder ]"
-echo "" | sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
-sudo apt update
-sudo apt install simplescreenrecorder -y
+bp_dirs=("$HOME/Desktop $HOME/Documents $HOME/Downloads $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos")
+for name in "${bp_dirs[@]}"; do
+	if [ -d "$name" ]; then
+		PROCESSING "[ Removing boilerplate home directories ]"
+		rmdir "$name"
+	fi
+done
 
-PROCESSING "[ Installing task ]"
-sudo apt install taskwarrior -y
+############################
+#   pip installations
+############################
+pip_progs=("requests flask flask-login colorama passlib pwntools netifaces iptools pyopenssl pydispatch scapy")
+for name in "${pip_progs[@]}"; do
+	PROCESSING "[ Installing $name ]"
+	pip3 install "$name"
+done
 
-PROCESSING "[ Installing pip3 ]"
-sudo apt install python-pip3 -y
-
-PROCESSING "[ Removing boilerplate home directories ]"
-rmdir ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
-
-PROCESSING "[ Installing guake ]"
-sudo apt install guake -y
-
-PROCESSING "[ Installing openvpn ]"
-sudo apt install openvpn -y
-
-PROCESSING "[ Installing nmap ]"
-sudo apt install nmap -y
-
-PROCESSING "[ Installing docker ]"
-sudo apt install -y docker.io
-sudo groupadd docker
-sudo usermod -aG docker "$(logname)"
-
-PROCESSING "[ Installing curl ]"
-sudo apt install curl -y
-
-PROCESSING "[ Installing pinta ]"
-sudo apt install pinta -y
-
-PROCESSING "[ Installing exiftool ]"
-sudo apt install exiftool -y
-
-PROCESSING "[ Installing Python PIL ]"
-sudo apt install python-pil -y
-
-PROCESSING "[ Installing sqlitebrowser ]"
-sudo apt install sqlitebrowser -y
-
-PROCESSING "[ Installing Wireshark ]"
-sudo apt install wireshark -y
-
-PROCESSING "[ Installing pefile ]"
-pip3 install pefile
-
+############################
+#   git installations
+############################
 PROCESSING "[ Downloading volatility3 ]"
 git clone https://github.com/volatilityfoundation/volatility3.git
 
-PROCESSING "[ Installing volatility ]"
-sudo snap install volatility-phocean
-
-PROCESSING "[ Install Real VNC Viewer ]"
-wget "https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.17.1113-Linux-x64.deb" -O vnc_viewer.deb
-dpkg -i vnc_viewer.deb
-rm vnc_viewer.deb
-
-PROCESSING "[ Install Real VNC Connect (Server) ]"
-wget 'https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.2.1-Linux-x64.deb' -O vnc_server.deb
-dpkg -i vnc_server.deb
-rm vnc_server.deb
-
-PROCESSING "[ Adding VNC Connect (Server) service to the default startup /etc/rc.local ]"
-grep "vncserver-x11-serviced.service" /etc/rc.local
-if [ $? -eq 1 ]; then
-	echo "systemctl start vncserver-x11-serviced.service" >>~/etc/rc.local
-fi
-
-PROCESSING "[ Installing Atom ]"
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-sudo apt update
-sudo apt install atom -y
-
-PROCESSING "[ Installing python-requests ]"
-pip3 install requests
-
-PROCESSING "[ Installing idle ]"
-sudo apt install -y idle
-
-PROCESSING "[ Installing xclip ]"
-sudo apt install -y xclip
-grep "alias xclip" ~/.bashrc
-if [ $? -eq 1 ]; then
-	echo "alias xclip='xclip -selection clipboard'" >>~/.bashrc
-fi
-
-PROCESSING "[ Installing Python flask ]"
-sudo pip3 install flask
-
-PROCESSING "[ Installing Python flask-login ]"
-sudo pip3 install flask-login
-
-PROCESSING "[ Installing Python colorama ]"
-sudo pip3 install colorama
-
-PROCESSING "[ Installing Python passlib ]"
-sudo pip3 install passlib
-
-PROCESSING "[ Installing Spotify ]"
-sudo snap install spotify
-
-PROCESSING "[ Installing Binwalk ]"
-sudo apt install binwalk -y
-
-PROCESSING "[ Installing Tesseract ]"
-sudo apt install tesseract-ocr -y
-
-PROCESSING "[ Installing foremost ]"
-sudo apt install foremost -y
-
-PROCESSING "[ Installing rot13 ]"
-sudo apt install bsdgames -y
-
-PROCESSING "[ Installing hexedit ]"
-sudo apt install hexedit -y
-
-PROCESSING "[ Installing Python pwntools ]"
-sudo pip3 install pwntools
-
-PROCESSING "[ Installing Go ]"
-sudo apt install golang-go -y
-sudo apt install gccgo-go -y
-PROCESSING "Adding GOPATH and GOBIN to .bashrc, so future installs are easy.."
+############################
+#   setup paths
+############################
+PROCESSING "[ Adding GOPATH and GOBIN to .bashrc ]"
 grep "export GOPATH" ~/.bashrc
 if [ $? -eq 1 ]; then
 	echo "export GOPATH=\$HOME/.go/" >>~/.bashrc
@@ -207,103 +173,9 @@ if [ $? -eq 1 ]; then
 	echo "export GOBIN=\$HOME/.go/bin" >>~/.bashrc
 	echo "export PATH=\$PATH:\$GOBIN" >>~/.bashrc
 fi
-
-PROCESSING "[ Installing gobuster ]"
-go get github.com/OJ/gobuster
-
-PROCESSING "[ Installing sqlite ]"
-sudo apt install sqlite -y
-
-PROCESSING "[ Installing nikto ]"
-sudo apt install nikto -y
-
-PROCESSING "[ Installing zbarimg ]"
-sudo apt install zbar-tools -y
-
-PROCESSING "[ Installing qrencode ]"
-sudo apt install qrencode -y
-
-PROCESSING "[ Installing pdfcrack ]"
-sudo apt install pdfcrack -y
-
-PROCESSING "[ Installing Virtualbox ]"
-sudo apt install virtualbox-qt -y
-
-PROCESSING "[ Installing Vagrant ]"
-sudo apt install vagrant -y
-
-# PROCESSING "[ Installing Hopper ]"
-# wget "https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.5.28-Linux.deb"
-# dpkg -i Hopper-v4-4.5.28-Linux.deb
-# rm Hopper-v4-4.5.28-Linux.deb
-
-PROCESSING "[ Installing Oracle Java 8 ]"
-echo "" | sudo add-apt-repository ppa:webupd8team/java
-sudo apt update
-sudo apt install oracle-java8-installer -y
-
-PROCESSING "Downloading stegsolve.jar ]"
-wget "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
-chmod +x "stegsolve.jar"
-
-PROCESSING "[ Installing fcrackzip ]"
-sudo apt install fcrackzip -y
-
-PROCESSING "[ Installing unrar ]"
-sudo apt install unrar -y
-
-PROCESSING "[ Installing 7zip ]"
-sudo apt install p7zip -y
-
-PROCESSING "[ Installing steghide ]"
-sudo apt install steghide -y
-
-PROCESSING "[ Installing hashcat ]"
-wget https://hashcat.net/files/hashcat-5.1.0.7z
-p7zip -d hashcat-5.1.0.7z
-cd hashcat-5.1.0 || exit
-cp hashcat64.bin /usr/bin/
-ln -s /usr/bin/hashcat64.bin /usr/bin/hashcat
-
-PROCESSING "[ Installing ffmpeg ]"
-sudo apt install ffmpeg -y
-
-PROCESSING "[ Installing Python library netifaces ]"
-sudo pip3 install netifaces
-
-PROCESSING "[ Installing Python library iptools ]"
-sudo pip3 install iptools
-
-PROCESSING "[ Installing Python library OpenSSL ]"
-sudo pip3 install pyopenssl
-
-PROCESSING "[ Installing Python library pydispatch ]"
-sudo pip3 install pydispatch
-
-PROCESSING "[ Installing GIMP ]"
-sudo apt install gimp -y
-
-PROCESSING "[ Installing cmake ]"
-sudo apt install cmake -y
-
-PROCESSING "[ Installing mplayer ]"
-sudo apt install mplayer -y
-
-PROCESSING "[ Installing sshpass ]"
-sudo apt install sshpass -y
-
-PROCESSING "[ Installing tcpflow ]"
-sudo apt install tcpflow -y
-
-PROCESSING "[ Installing Python scapy ]"
-sudo pip3 install scapy
-
-PROCESSING "[ Installing the thing that 7z2john.pl needs ]"
-sudo apt install libcompress-raw-lzma-perl
-
-PROCESSING "[ Installing dos2unix ]"
-sudo apt install libcompress-raw-lzma-perl
-
 PROCESSING "[ Updating Prompt ]"
 alias brc='source ~/.bashrc'
 brc
+
+PROCESSING "[ Installing gobuster ]"
+go get github.com/OJ/gobuster
