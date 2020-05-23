290901c44b37b3184592b735f01ef284719f3a66
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-05-23 07:02:10 -0400

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,206 +1,203 @@
 #!/bin/bash
 
-# Credit: JohnHammond
+# CERRORit: JohnHammond
 # https://github.com/JohnHammond/ignition_key/blob/master/ignition_key.sh
 
 # Define colors ]
-RED=$(tput bold && tput setaf 1)
-GREEN=$(tput bold && tput setaf 2)
-YELLOW=$(tput bold && tput setaf 3)
-BLUE=$(tput bold && tput setaf 4)
-NC=$(tput sgr0)
+ERROR=$(tput bold && tput setaf 1)
+SUCCESS=$(tput bold && tput setaf 2)
+WARNING=$(tput bold && tput setaf 3)
+PROCESSING=$(tput bold && tput setaf 6)
+RESET=$(tput sgr0)
 
-update(){
+UPDATE(){
     sudo apt update
+    sudo apt upgrade -y
 }
 
-function RED() {
-	echo -e "\n${RED}${1}${NC}"
+ERROR() {
+	echo -e "\n${ERROR}${1}${RESET}"
 }
-function GREEN() {
-	echo -e "\n${GREEN}${1}${NC}"
+SUCCESS() {
+	echo -e "\n${SUCCESS}${1}${RESET}"
 }
-function YELLOW() {
-	echo -e "\n${YELLOW}${1}${NC}"
+WARNING() {
+	echo -e "\n${WARNING}${1}${RESET}"
 }
-function BLUE() {
-	echo -e "\n${BLUE}${1}${NC}"
+PROCESSING() {
+	echo -e "\n${PROCESSING}${1}${RESET}"
 }
 
 # Testing if root ]
 if [ $UID -ne 0 ]; then
-	RED "You must run this script as root!" && echo
+	ERROR "You must run this script as root!" && echo
 	exit
 fi
 
 
-BLUE "[ Updating repositories ]"
-sudo apt update
+PROCESSING "[ Updating repositories ]"
+UPDATE
 
-BLUE "[ Installing Snap ]"
+PROCESSING "[ Installing Snap ]"
 sudo apt install snapd
 
-BLUE "[ Installing Visual Studio Code ]"
+PROCESSING "[ Installing Visual Studio Code ]"
 sudo apt install software-properties-common apt-transport-https -y
 
-BLUE "[ Importing the Microsoft GPG key ]"
+PROCESSING "[ Importing the Microsoft GPG key ]"
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 
-BLUE "[ Enabling the Visual Studio Code repository and install ]"
+PROCESSING "[ Enabling the Visual Studio Code repository and install ]"
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code -y
 
-BLUE "[ Installing git ]"
+PROCESSING "[ Installing git ]"
 sudo apt install git -y
 
-BLUE "[ Installing Sublime Text ]" # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
+PROCESSING "[ Installing Sublime Text ]" # according to https://www.sublimetext.com/docs/3/linux_repositories.html-
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install sublime-text -y
 
-BLUE "[ Installing terminator ]"
+PROCESSING "[ Installing terminator ]"
 sudo apt install terminator -y
 
-BLUE "[ Setting terminator as the default terminal emulator ]"
+PROCESSING "[ Setting terminator as the default terminal emulator ]"
 sed -i s/Exec=gnome-terminal/Exec=terminator/g /usr/share/applications/gnome-terminal.desktop
 
-BLUE "[ Forcing a color prompt in ~/.bashrc ]"
-grep "export PS1" ~/.bashrc
-if [ $? -eq 1 ]; then
-	echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[38;5;11m\]\u\[$(tput sgr0)\]@\h:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]\[$(tput sgr0)\]: \[$(tput sgr0)\]'" >>~/.bashrc
-	# echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >>~/.bashrc
-fi
+PROCESSING "[ Forcing color prompt in ~/.bashrc ]"
+echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[38;5;11m\]\u\[$(tput sgr0)\]@\h:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]\[$(tput sgr0)\]: \[$(tput sgr0)\]'" >>~/.bashrc
 
-BLUE "[ Installing SimpleScreenRecorder ]"
+PROCESSING "[ Installing SimpleScreenRecorder ]"
 echo "" | sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
 sudo apt update
 sudo apt install simplescreenrecorder -y
 
-BLUE "[ Installing task ]"
+PROCESSING "[ Installing task ]"
 sudo apt install taskwarrior -y
 
-BLUE "[ Installing pip3 ]"
+PROCESSING "[ Installing pip3 ]"
 sudo apt install python-pip3 -y
 
-BLUE "[ Removing boilerplate home directories ]"
+PROCESSING "[ Removing boilerplate home directories ]"
 rmdir ~/Desktop ~/Documents ~/Downloads ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
 
-BLUE "[ Installing guake ]"
+PROCESSING "[ Installing guake ]"
 sudo apt install guake -y
 
-BLUE "[ Installing openvpn ]"
+PROCESSING "[ Installing openvpn ]"
 sudo apt install openvpn -y
 
-BLUE "[ Installing nmap ]"
+PROCESSING "[ Installing nmap ]"
 sudo apt install nmap -y
 
-BLUE "[ Installing docker ]"
+PROCESSING "[ Installing docker ]"
 sudo apt install -y docker.io
 sudo groupadd docker
 sudo usermod -aG docker "$(logname)"
 
-BLUE "[ Installing curl ]"
+PROCESSING "[ Installing curl ]"
 sudo apt install curl -y
 
-BLUE "[ Installing pinta ]"
+PROCESSING "[ Installing pinta ]"
 sudo apt install pinta -y
 
-BLUE "[ Installing exiftool ]"
+PROCESSING "[ Installing exiftool ]"
 sudo apt install exiftool -y
 
-BLUE "[ Installing Python PIL ]"
+PROCESSING "[ Installing Python PIL ]"
 sudo apt install python-pil -y
 
-BLUE "[ Installing sqlitebrowser ]"
+PROCESSING "[ Installing sqlitebrowser ]"
 sudo apt install sqlitebrowser -y
 
-BLUE "[ Installing Wireshark ]"
+PROCESSING "[ Installing Wireshark ]"
 sudo apt install wireshark -y
 
-BLUE "[ Installing pefile ]"
+PROCESSING "[ Installing pefile ]"
 pip3 install pefile
 
-BLUE "[ Downloading volatility3 ]"
+PROCESSING "[ Downloading volatility3 ]"
 git clone https://github.com/volatilityfoundation/volatility3.git
 
-BLUE "[ Installing volatility ]"
+PROCESSING "[ Installing volatility ]"
 sudo snap install volatility-phocean
 
-BLUE "[ Install Real VNC Viewer ]"
+PROCESSING "[ Install Real VNC Viewer ]"
 wget "https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.17.1113-Linux-x64.deb" -O vnc_viewer.deb
 dpkg -i vnc_viewer.deb
 rm vnc_viewer.deb
 
-BLUE "[ Install Real VNC Connect (Server) ]"
+PROCESSING "[ Install Real VNC Connect (Server) ]"
 wget 'https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.2.1-Linux-x64.deb' -O vnc_server.deb
 dpkg -i vnc_server.deb
 rm vnc_server.deb
 
-BLUE "[ Adding VNC Connect (Server) service to the default startup /etc/rc.local ]"
+PROCESSING "[ Adding VNC Connect (Server) service to the default startup /etc/rc.local ]"
 grep "vncserver-x11-serviced.service" /etc/rc.local
 if [ $? -eq 1 ]; then
 	echo "systemctl start vncserver-x11-serviced.service" >>~/etc/rc.local
 fi
 
-BLUE "[ Installing Atom ]"
+PROCESSING "[ Installing Atom ]"
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt update
 sudo apt install atom -y
 
-BLUE "[ Installing python-requests ]"
+PROCESSING "[ Installing python-requests ]"
 pip3 install requests
 
-BLUE "[ Installing idle ]"
+PROCESSING "[ Installing idle ]"
 sudo apt install -y idle
 
-BLUE "[ Installing xclip ]"
+PROCESSING "[ Installing xclip ]"
 sudo apt install -y xclip
 grep "alias xclip" ~/.bashrc
 if [ $? -eq 1 ]; then
 	echo "alias xclip='xclip -selection clipboard'" >>~/.bashrc
 fi
 
-BLUE "[ Installing Python flask ]"
+PROCESSING "[ Installing Python flask ]"
 sudo pip3 install flask
 
-BLUE "[ Installing Python flask-login ]"
+PROCESSING "[ Installing Python flask-login ]"
 sudo pip3 install flask-login
 
-BLUE "[ Installing Python colorama ]"
+PROCESSING "[ Installing Python colorama ]"
 sudo pip3 install colorama
 
-BLUE "[ Installing Python passlib ]"
+PROCESSING "[ Installing Python passlib ]"
 sudo pip3 install passlib
 
-BLUE "[ Installing Spotify ]"
+PROCESSING "[ Installing Spotify ]"
 sudo snap install spotify
 
-BLUE "[ Installing Binwalk ]"
+PROCESSING "[ Installing Binwalk ]"
 sudo apt install binwalk -y
 
-BLUE "[ Installing Tesseract ]"
+PROCESSING "[ Installing Tesseract ]"
 sudo apt install tesseract-ocr -y
 
-BLUE "[ Installing foremost ]"
+PROCESSING "[ Installing foremost ]"
 sudo apt install foremost -y
 
-BLUE "[ Installing rot13 ]"
+PROCESSING "[ Installing rot13 ]"
 sudo apt install bsdgames -y
 
-BLUE "[ Installing hexedit ]"
+PROCESSING "[ Installing hexedit ]"
 sudo apt install hexedit -y
 
-BLUE "[ Installing Python pwntools ]"
+PROCESSING "[ Installing Python pwntools ]"
 sudo pip3 install pwntools
 
-BLUE "[ Installing Go ]"
+PROCESSING "[ Installing Go ]"
 sudo apt install golang-go -y
 sudo apt install gccgo-go -y
-BLUE "Adding GOPATH and GOBIN to .bashrc, so future installs are easy.."
+PROCESSING "Adding GOPATH and GOBIN to .bashrc, so future installs are easy.."
 grep "export GOPATH" ~/.bashrc
 if [ $? -eq 1 ]; then
 	echo "export GOPATH=\$HOME/.go/" >>~/.bashrc
@@ -211,98 +208,102 @@ if [ $? -eq 1 ]; then
 	echo "export PATH=\$PATH:\$GOBIN" >>~/.bashrc
 fi
 
-BLUE "[ Installing gobuster ]"
+PROCESSING "[ Installing gobuster ]"
 go get github.com/OJ/gobuster
 
-BLUE "[ Installing sqlite ]"
+PROCESSING "[ Installing sqlite ]"
 sudo apt install sqlite -y
 
-BLUE "[ Installing nikto ]"
+PROCESSING "[ Installing nikto ]"
 sudo apt install nikto -y
 
-BLUE "[ Installing zbarimg ]"
+PROCESSING "[ Installing zbarimg ]"
 sudo apt install zbar-tools -y
 
-BLUE "[ Installing qrencode ]"
+PROCESSING "[ Installing qrencode ]"
 sudo apt install qrencode -y
 
-BLUE "[ Installing pdfcrack ]"
+PROCESSING "[ Installing pdfcrack ]"
 sudo apt install pdfcrack -y
 
-BLUE "[ Installing Virtualbox ]"
+PROCESSING "[ Installing Virtualbox ]"
 sudo apt install virtualbox-qt -y
 
-BLUE "[ Installing Vagrant ]"
+PROCESSING "[ Installing Vagrant ]"
 sudo apt install vagrant -y
 
-# BLUE "[ Installing Hopper ]"
+# PROCESSING "[ Installing Hopper ]"
 # wget "https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.5.28-Linux.deb"
 # dpkg -i Hopper-v4-4.5.28-Linux.deb
 # rm Hopper-v4-4.5.28-Linux.deb
 
-BLUE "[ Installing Oracle Java 8 ]"
+PROCESSING "[ Installing Oracle Java 8 ]"
 echo "" | sudo add-apt-repository ppa:webupd8team/java
 sudo apt update
 sudo apt install oracle-java8-installer -y
 
-BLUE "Downloading stegsolve.jar ]"
+PROCESSING "Downloading stegsolve.jar ]"
 wget "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
 chmod +x "stegsolve.jar"
 
-BLUE "[ Installing fcrackzip ]"
+PROCESSING "[ Installing fcrackzip ]"
 sudo apt install fcrackzip -y
 
-BLUE "[ Installing unrar ]"
+PROCESSING "[ Installing unrar ]"
 sudo apt install unrar -y
 
-BLUE "[ Installing 7zip ]"
+PROCESSING "[ Installing 7zip ]"
 sudo apt install p7zip -y
 
-BLUE "[ Installing steghide ]"
+PROCESSING "[ Installing steghide ]"
 sudo apt install steghide -y
 
-BLUE "[ Installing hashcat ]"
+PROCESSING "[ Installing hashcat ]"
 wget https://hashcat.net/files/hashcat-5.1.0.7z
 p7zip -d hashcat-5.1.0.7z
 cd hashcat-5.1.0 || exit
 cp hashcat64.bin /usr/bin/
 ln -s /usr/bin/hashcat64.bin /usr/bin/hashcat
 
-BLUE "[ Installing ffmpeg ]"
+PROCESSING "[ Installing ffmpeg ]"
 sudo apt install ffmpeg -y
 
-BLUE "[ Installing Python library netifaces ]"
+PROCESSING "[ Installing Python library netifaces ]"
 sudo pip3 install netifaces
 
-BLUE "[ Installing Python library iptools ]"
+PROCESSING "[ Installing Python library iptools ]"
 sudo pip3 install iptools
 
-BLUE "[ Installing Python library OpenSSL ]"
+PROCESSING "[ Installing Python library OpenSSL ]"
 sudo pip3 install pyopenssl
 
-BLUE "[ Installing Python library pydispatch ]"
+PROCESSING "[ Installing Python library pydispatch ]"
 sudo pip3 install pydispatch
 
-BLUE "[ Installing GIMP ]"
+PROCESSING "[ Installing GIMP ]"
 sudo apt install gimp -y
 
-BLUE "[ Installing cmake ]"
+PROCESSING "[ Installing cmake ]"
 sudo apt install cmake -y
 
-BLUE "[ Installing mplayer ]"
+PROCESSING "[ Installing mplayer ]"
 sudo apt install mplayer -y
 
-BLUE "[ Installing sshpass ]"
+PROCESSING "[ Installing sshpass ]"
 sudo apt install sshpass -y
 
-BLUE "[ Installing tcpflow ]"
+PROCESSING "[ Installing tcpflow ]"
 sudo apt install tcpflow -y
 
-BLUE "[ Installing Python scapy ]"
+PROCESSING "[ Installing Python scapy ]"
 sudo pip3 install scapy
 
-BLUE "[ Installing the thing that 7z2john.pl needs ]"
+PROCESSING "[ Installing the thing that 7z2john.pl needs ]"
 sudo apt install libcompress-raw-lzma-perl
 
-BLUE "[ Installing dos2unix ]"
+PROCESSING "[ Installing dos2unix ]"
 sudo apt install libcompress-raw-lzma-perl
+
+PROCESSING "[ Updating Prompt ]"
+alias brc='source ~/.bashrc'
+brc
