386cfad6ba1d165a9077ce10ea4c322151b9364f
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-07-31 08:27:43 -0400

    Update update_repo.bat

--- update_repo/update_repo.bat
+++ update_repo/update_repo.bat
@@ -6,14 +6,38 @@ set REPO=%2
 rem Must provide at least two arguments
 if "%2" == "" (
     echo.
-    echo Usage: %0 username repo
+    echo Usage: %0 ^<USER^> ^<REPO^>
     exit /b
 ) else (
+    goto backup
+)
+
+:backup
+rem Check if 7zip is installed
+if not exist "C:\Program Files\7-Zip\7z.exe" (
+    echo.
+    echo Error: Script requires 7zip but it's not installed.
+    exit /b
+) else (
+    cls
+    echo.
+    set /p BACKUPDIR=Specify backup file path ^(e.g: %USERPROFILE%\^): 
+    set hr=%time:~0,2%
+    if %hr% lss 10 SET hr=0%hr:~1,1%
+    set TODAY=%date:~7,2%-%date:~4,2%-%date:~10,4%-%hr%%time:~3,2%%time:~6,2%%time:~9,2%
+    
+    rem Create repo backup
+    set SRC=%~dp0
+    echo [+] Backing up %SRC%
+    "C:\Program Files\7-Zip\7z.exe" a -tzip "%BACKUPDIR%\%REPO%_repo_%TODAY%.zip" %SRC% -mx5 >NUL 2>&1
+    echo [+] Backup finished!
     goto gitrun
 )
 
 :gitrun
-Remove all files from git cache
+rem Remove all files from git cache
+echo [+] Initiating repo reset
+echo.
 call git rm -r --cached .
 call git add .
 call git commit -am "Refreshing .gitignore"
@@ -38,3 +62,6 @@ call git remote set-url origin git@github.com:%USER%/%REPO%.git
 
 rem Finally, force update to our repository:
 call git push -f origin master
+
+echo.
+echo Done!
