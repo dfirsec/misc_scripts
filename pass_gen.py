07393c1a65888a4dc4b8177149f2a3a2a8939114
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-09-30 10:57:53 -0400

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,14 +1,29 @@
-#!/usr/bin/env python
+import secrets
+import string
+import sys
 
 __description__ = "Python Password Generator"
 
-import string
-import secrets
+try:
+    from colorama import Fore, init
+    # initialize colorama
+    init()
+except ImportError:
+    sys.exit("Please install colorama: pip install colorama --user")
 
 
-def str_gen(str_len=10):
+def str_gen(str_len):
     s = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(secrets.choice(s) for i in range(str_len))
+    return ''.join(secrets.choice(s) for _ in range(int(str_len)))
 
 
-print(str_gen())
+if len(sys.argv) < 2:
+    sys.exit("[ERROR] Provide a password length.")
+else:
+    try:
+        if int(sys.argv[1]):
+            if int(sys.argv[1]) > 50:
+                sys.exit(f"{Fore.YELLOW}[WARNING]{Fore.RESET} Whew, that's too long!")  # nopep8
+            print(f"{Fore.CYAN}{str_gen(sys.argv[1])}{Fore.RESET}")
+    except ValueError:
+        sys.exit(f"{Fore.RED}[ERROR]{Fore.RESET} Must use an integer")
